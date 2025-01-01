242a7522a349df0733ee7f7daff67a466b1f249a
AwakeEva <eva@akamo.info>
2025-01-01 15:12:42 +0300

    Logic Temperature and Multi Projectile

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -18,9 +18,20 @@ namespace UnitBrains.Player
             float overheatTemperature = OverheatTemperature;
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+            ///////////////////////////////////////
+
+            int currentTemperature = GetTemperature();
+
+            if (currentTemperature >= overheatTemperature)
+                return;
+            
+            for (int i = 0; i <= currentTemperature; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            
+            IncreaseTemperature();
             ///////////////////////////////////////
         }
 
